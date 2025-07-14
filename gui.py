c742e860f5686db62cf0adb78ee2d4903d830692
dragonbough <fiveteen81@gmail.com>
2025-07-14 14:57:24 +0100

    created primitive note network display in PyQT
    - created NoteNetwork class in frontend using pyvis
    - Notes in the network can be displayed via the NodeNetworkView class,
    using QWebEngineView in PyQT
    - implemented basic network view functionality in the main window
    - other small additions (e.g showing window only when all data is
    loaded)

--- gui.py
+++ gui.py
@@ -3,27 +3,85 @@ import app
 from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from pyvis import network
 
-class NoteItem(QTextDocument):
+# class NoteItem(QTextDocument):
     
-    def __init__(self, id : str):
+#     def __init__(self, id : str):
         
-        self.note =  app.Note(id)
+#         self.note =  app.Note(id)
 
+class NoteNetwork():
+    
+    def __init__(self, notes : list[app.Note] = app.user_notes.get()):
+        self.notes = notes
+        self.network = network.Network()
+        for note in self.notes:
+            self.network.add_node(note.name)
+        for note in self.notes:
+            for note_reference in note.references:
+                self.network.add_edge(note.name, note_reference)
+    
+    def return_html(self, physics : bool = True):
+        self.network.toggle_physics(physics)
+        self.network.generate_html()
+        return self.network.html
+    
+class NoteNetworkView(QWebEngineView):
+    
+    def __init__(self, note_network : NoteNetwork, physics : bool = True, debug : bool = False):
+        super().__init__()
+        
+        self.setZoomFactor(0.65)
+        self.note_network = note_network
+        
+        if debug:
+            self.loadStarted.connect(lambda : print("loading started"))
+            self.loadProgress.connect(lambda progress : print(f"loading {progress}%"))
+            self.loadFinished.connect(lambda : print("loading finished"))
+        
+        self.render(physics)
+    
+    def render(self, physics : bool = True):
+        self.setHtml(self.note_network.return_html(physics))
+    
+        
 class Window(QMainWindow):
     
     def __init__(self):
         super().__init__()
         
+        self.shown = None
+        
         self.layout = QVBoxLayout()
+        
         self.texteditor = QTextEdit()
+        self.note_network = NoteNetwork()
+        self.network_view = NoteNetworkView(self.note_network, debug=True, physics=False)
+    
+        # self.network_view.loadStarted.connect(self.hide)
+        # self.network_view.loadFinished.connect(lambda : self.show() if self.shown else self.hide())
+        
+        self.layout.addWidget(self.network_view)
         self.layout.addWidget(self.texteditor)
+        
         self.widget = QWidget()
         self.widget.setLayout(self.layout)
         self.setCentralWidget(self.widget)
+    
+    #sets shown to true -- window may still not be shown as loading isnt finished but once done it will know what to set it to
+    def show(self, *args):
+        self.shown = True
+        super().show(*args)
         
-app = QApplication(sys.argv)
+    
+Application = QApplication(sys.argv)
 window = Window()
+
 window.show()
 
-app.exec()
+Application.exec()
+
+
+
