c1369a2aaf3c321de6a71870129499160d8f531d
dragonbough <fiveteen81@gmail.com>
2025-07-18 13:48:11 +0100

    working graph visualisation of note network in tab system
    - added options to make for a better network visualisation using
    set_options()
    - cdn_resources used inline to allow for offline access
    - small changes to make for a better visualisation:
    	- removed border around qwebengineview
    	- hid scroll bars with flags
    - implemented tab system to represent the graph and editor separately
    - other small fixes

--- gui.py
+++ gui.py
@@ -4,71 +4,134 @@ from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtWebEngineCore import QWebEngineSettings
+from PyQt6.QtWebChannel import *
 from pyvis import network
-
-# class NoteItem(QTextDocument):
-    
-#     def __init__(self, id : str):
-        
-#         self.note =  app.Note(id)
 
 class NoteNetwork():
     
-    def __init__(self, notes : list[app.Note] = app.user_notes.get()):
+    def __init__(self, notes : list[app.Note] = app.user_notes.get(), font_color : str = "white", bg_color : str = None, screen_max_height : int = 1080):
         self.notes = notes
-        self.network = network.Network()
+        self.bg_color = bg_color
+        # cdn resources being in line means i dont need to worry about access to the internet
+        self.network = network.Network(font_color=font_color, cdn_resources="in_line", bgcolor=self.bg_color, height=screen_max_height)
+        
         for note in self.notes:
             self.network.add_node(note.name)
         for note in self.notes:
             for note_reference in note.references:
                 self.network.add_edge(note.name, note_reference)
+                
+                            
+    #sets the options for the nodes and edges in the network
+    def update_options(self):
+        self.network.set_options(
+            """"" 
+            const options = {
+                
+            "configure": {
+                "enabled": false
+            },
+            "nodes": {
+                "opacity": 1,
+                "fixed": {
+                "x": false,
+                "y": false
+                }
+            },
+            "edges": {
+                "arrows": {
+                "to": {
+                    "enabled": true
+                }
+                },
+                "color": {
+                "inherit": true
+                },
+                "selfReference": {
+                "angle": 0.7853981633974483
+                },
+                "smooth": false
+            },
+            "interaction": {
+                "selectConnectedEdges": false,
+                "hoverConnectedEdges": false
+            },
+            "physics": {
+                "barnesHut": {
+                "avoidOverlap": 0.8
+                },
+                "minVelocity": 0.75
+            }
+            }
+            """""
+        )
+    
     
     def return_html(self, physics : bool = True):
         self.network.toggle_physics(physics)
-        self.network.generate_html()
-        return self.network.html
+        self.update_options()
+        html = self.network.generate_html()
+        #adds styling to html to remove the white border around the network visualisation
+        html = html.replace("<head>", f"<head><style>html, body {{ margin: 0; padding: 0; background-color: {self.bg_color}; }} </style>")
+        
+        return html
     
 class NoteNetworkView(QWebEngineView):
     
     def __init__(self, note_network : NoteNetwork, physics : bool = True, debug : bool = False):
         super().__init__()
         
-        self.setZoomFactor(0.65)
-        self.note_network = note_network
-        
+        self.physics = physics
+        #hides scroll bars
+        self.settings().setAttribute(QWebEngineSettings.WebAttribute.ShowScrollBars, False)
+        self.setZoomFactor(1)
+        self.note_network = note_network        
         if debug:
             self.loadStarted.connect(lambda : print("loading started"))
             self.loadProgress.connect(lambda progress : print(f"loading {progress}%"))
             self.loadFinished.connect(lambda : print("loading finished"))
         
-        self.render(physics)
+        self.render()
     
-    def render(self, physics : bool = True):
-        self.setHtml(self.note_network.return_html(physics))
+    def render(self):
+        self.setHtml(self.note_network.return_html(self.physics))
     
         
 class Window(QMainWindow):
     
-    def __init__(self):
+    def __init__(self, max_screen_height : int = 1080):
         super().__init__()
         
         self.shown = None
         
-        self.layout = QVBoxLayout()
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
         
+        self.tab1layout = QVBoxLayout()
+        self.tab1layout.setContentsMargins(0, 0, 0, 0)
+        self.tab1layout.setSpacing(0)
+        self.tab2layout = QVBoxLayout()
+        
+        self.tab1.setLayout(self.tab1layout)
+        self.tab2.setLayout(self.tab2layout)
+                
         self.texteditor = QTextEdit()
-        self.note_network = NoteNetwork()
+        self.note_network = NoteNetwork(bg_color=self.palette().window().color().name(), screen_max_height=max_screen_height)
         self.network_view = NoteNetworkView(self.note_network, debug=True, physics=False)
-    
+        
         # self.network_view.loadStarted.connect(self.hide)
         # self.network_view.loadFinished.connect(lambda : self.show() if self.shown else self.hide())
         
-        self.layout.addWidget(self.network_view)
-        self.layout.addWidget(self.texteditor)
+        self.tab1layout.addWidget(self.network_view)
+        self.tab2layout.addWidget(self.texteditor)
         
-        self.widget = QWidget()
-        self.widget.setLayout(self.layout)
-        self.setCentralWidget(self.widget)
+        self.tabs.addTab(self.tab1, "Note Network")
+        self.tabs.addTab(self.tab2, "Note Editor")
+        
+        self.setCentralWidget(self.tabs)
+        
     
     #sets shown to true -- window may still not be shown as loading isnt finished but once done it will know what to set it to
     def show(self, *args):
@@ -77,11 +140,9 @@ class Window(QMainWindow):
         
     
 Application = QApplication(sys.argv)
-window = Window()
+height = Application.primaryScreen().size().height()
+window = Window(height)
 
 window.show()
 
 Application.exec()
-
-
-
